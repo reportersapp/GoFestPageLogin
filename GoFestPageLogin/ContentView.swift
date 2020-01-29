a4f68150b98927d4cc0303b2c836233a9f3a5ea0
Mortaza Behboudi <mortazabehboudi@gmail.com>
2020-01-29 10:17:33 +0100

    Initial Commit

---
+++ GoFestPageLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoFestPageLogin
+//
+//  Created by Mortaza Behboudi on 29/01/2020.
+//  Copyright © 2020 GoFest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
